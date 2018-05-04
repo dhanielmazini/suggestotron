21e2145fc8f52bbb6376b0b1cf8bdc4c00d4bb49
Dhaniel Mazini <dnmazini@gmail.com>
2018-05-04 15:35:51 -0300

    Conexão com o BD implementada

---
+++ public/TopicData.php
@@ -0,0 +1,40 @@
+<?php
+class TopicData {
+    protected $connection;
+
+    
+    public function connect(){
+        $this->connection = new PDO("mysql:host=localhost;dbname=suggestotron", "root", "root");
+    }
+    
+    public function __construct(){
+        $this->connect();
+    }
+    
+    public function getAllTopics(){
+        $query = $this->connection->prepare("SELECT * FROM topics");
+        $query->execute();
+
+        return $query;
+    }
+
+    public function add($data){
+        $query = $this->connection->prepare(
+            "INSERT INTO topics (
+                title,
+                description
+            ) VALUES (
+                :title,
+                :description
+            )"
+        );
+
+        $data = [
+            ':title' => $data['title'],
+            ':description' => $data['description']
+        ];
+
+        $query->execute($data);
+    }
+}
+?>
---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+    require 'TopicData.php';
+
+    $data = new TopicData();
+    $data->connect();
+    
+    $topics = $data->getAllTopics();
+
+    foreach ($topics as $topic) {
+        echo "<h3>" .$topic['title']. " (ID: " .$topic['id']. ")</h3>";
+        echo "<p>";
+        echo nl2br($topic['description']);
+        echo "</p>";
+    }
+?>
